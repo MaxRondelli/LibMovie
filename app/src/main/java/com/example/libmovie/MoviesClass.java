79fd31cd41e7344aea8b1b0a31c2ab935e98c074
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2021-10-12 12:28:06 +0300

    Commit di prova

--- app/src/main/java/com/example/libmovie/MoviesClass.java
+++ app/src/main/java/com/example/libmovie/MoviesClass.java
@@ -3,9 +3,9 @@ package com.example.libmovie;
 public class MoviesClass {
 
     String nome;
-    String id;
 
-    int x = 9;
+    int numero = 0;
+
     public MoviesClass( String nome ){
         this.nome = nome;
 
